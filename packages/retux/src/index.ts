329150f701a9165769b595293bd9ab34328c7f06
crimx <straybugs@gmail.com>
2019-12-31 08:40:33 +0800

    refactor(retux): export createActionCreators

--- packages/retux/src/index.ts
+++ packages/retux/src/index.ts
@@ -1,10 +1,17 @@
-export { Action, ActionHandler, ActionHandlers, createReducer } from './basic'
+export {
+  Action,
+  ActionHandler,
+  ActionHandlers,
+  createReducer,
+  createActionCreators
+} from './basic'
 export {
   Action as FSA,
   ActionError as FSAError,
   ActionHandler as FSAHandler,
   ActionHandlers as FSAHandlers,
-  createReducer as createFSAReducer
+  createReducer as createFSAReducer,
+  createActionCreators as createFSACreators
 } from './fsa'
 export { MapStateToProps, MapDispatchToProps } from './react-redux'
 export { mergeUniqueObjects } from './merge-unique-objects'
