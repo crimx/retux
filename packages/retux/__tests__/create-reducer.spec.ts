f25c1b5814172923148239b8e7609c1147bd17c7
crimx <straybugs@gmail.com>
2020-01-01 22:00:07 +0800

    test(retux): add create-reducer spec

---
+++ packages/retux/__tests__/create-reducer.spec.ts
@@ -0,0 +1,63 @@
+import { createStore } from 'redux'
+import { createReducer } from '../src/create-reducer'
+import { ActionHandlers } from '../src/basic'
+import { ActionHandlers as FSAHandlers } from '../src/fsa'
+import { CreateActionCatalog } from '../src/utils'
+
+describe('create-reducer', () => {
+  it('should create Redux compatible reducer from any action handlers', () => {
+    const actionHandlers = {
+      ACTION1: (state: number) => state + 1,
+      ACTION2: (state: number, action: { payload: number }) =>
+        state + action.payload
+    }
+
+    const store = createStore(createReducer(0, actionHandlers))
+
+    store.dispatch({ type: 'ACTION1' })
+    expect(store.getState()).toBe(1)
+
+    store.dispatch({ type: 'ACTION2', payload: 2 })
+    expect(store.getState()).toBe(3)
+  })
+
+  it('should create Redux compatible reducer from basic action handlers', () => {
+    type ActionCatalog = CreateActionCatalog<{
+      ACTION1: {}
+      ACTION2: { payload: number }
+    }>
+
+    const actionHandlers: ActionHandlers<number, ActionCatalog> = {
+      ACTION1: state => state + 1,
+      ACTION2: (state, action) => state + action.payload
+    }
+
+    const store = createStore(createReducer(0, actionHandlers))
+
+    store.dispatch({ type: 'ACTION1' })
+    expect(store.getState()).toBe(1)
+
+    store.dispatch({ type: 'ACTION2', payload: 2 })
+    expect(store.getState()).toBe(3)
+  })
+
+  it('should create Redux compatible reducer from flux action handlers', () => {
+    type ActionCatalog = CreateActionCatalog<{
+      ACTION1: {}
+      ACTION2: { payload: number }
+    }>
+
+    const actionHandlers: FSAHandlers<number, ActionCatalog> = {
+      ACTION1: state => state + 1,
+      ACTION2: (state, action) => state + (action.error ? 1 : action.payload)
+    }
+
+    const store = createStore(createReducer(0, actionHandlers))
+
+    store.dispatch({ type: 'ACTION1' })
+    expect(store.getState()).toBe(1)
+
+    store.dispatch({ type: 'ACTION2', payload: 2 })
+    expect(store.getState()).toBe(3)
+  })
+})
