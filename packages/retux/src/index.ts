98111895b415d552e747dac2faa227941b806254
crimx <straybugs@gmail.com>
2020-01-06 00:00:54 +0800

    refactor(retux): remove react-retux from retux

--- packages/retux/src/index.ts
+++ packages/retux/src/index.ts
@@ -11,7 +11,6 @@ export {
   ActionHandlers as FSAHandlers,
   createActionCreators as createFSACreators
 } from './fsa'
-export { MapStateToProps, MapDispatchToProps } from './react-redux'
 export { combineUniqueObjects } from './combine-unique-objects'
 export { createReducer } from './create-reducer'
 export { CreateActionCatalog, ActionType } from './utils'
