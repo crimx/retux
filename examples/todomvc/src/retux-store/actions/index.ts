f51c90d53edd48d7215d2b0bcaef4d249c0fd96e
crimx <straybugs@gmail.com>
2020-01-05 13:01:06 +0800

    refactor(todomvc): add regular action creator

--- examples/todomvc/src/retux-store/actions/index.ts
+++ examples/todomvc/src/retux-store/actions/index.ts
@@ -1,21 +1,29 @@
-import {
-  createActionCreators,
-  combineUniqueObjects,
-  ActionHandlers
-} from 'retux'
+import { createActionCreators, combineUniqueObjects } from 'retux'
 import { actionHandlers as todoActionHandlers } from '../modules/todos'
 import { actionHandlers as visibilityFilterActionHandlers } from '../modules/visibilityFilter'
+import { StoreAction } from '../index'
 
 export const action = createActionCreators(
   // Auto-generated Action creators!
   // Strongly typed and also customizable.
+  // Also if you don't use Redux's combineReducers you can move this code to
+  // the index and create a single combined state and reducer for all modules
+  // then export the combined action handlers back here.
   combineUniqueObjects(visibilityFilterActionHandlers, todoActionHandlers),
   // Custom action creators start with lowercase which are easy to identify
   {
-    editTodo: (id: string, text: string) =>
-      ({
-        type: 'TodosEdit',
-        payload: { id, text }
-      } as const)
+    editTodo: (id: string, text: string): StoreAction<'TodosEdit'> => ({
+      type: 'TodosEdit',
+      payload: { id, text }
+    })
   }
 )
+
+// You can also write in the regular format
+export const editTodo = (
+  id: string,
+  text: string
+): StoreAction<'TodosEdit'> => ({
+  type: 'TodosEdit',
+  payload: { id, text }
+})
