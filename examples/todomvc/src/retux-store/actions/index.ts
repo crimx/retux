8d6925f17b761a1926861d540ab3ed5f8cd208e2
crimx <straybugs@gmail.com>
2020-01-07 22:33:29 +0800

    refactor(todomvc): update imports

--- examples/todomvc/src/retux-store/actions/index.ts
+++ examples/todomvc/src/retux-store/actions/index.ts
@@ -1,11 +1,28 @@
-import { defineActionCreators } from 'retux'
+import { proxyActionCreators } from 'retux'
 import { StoreAction, StoreActionCatalog } from '../index'
 
-// This proxies all the action creators!
-// For older browser Retux also offers `createActionCreators`.
-export const action = defineActionCreators<StoreActionCatalog>()
+// With Proxy an Action Creator is only created
+// on the first visit and be cached for subsequence visits.
+// For older browser Retux also offers
+// `createActionCreators` and `declareActionCreators`.
+export const action = proxyActionCreators<StoreActionCatalog>()
 
-// Custom action creators
+// You can also rewire custom action creators with anything.
+const rewiredActionCreators = {
+  // Also useful if you want to replace an Action Creator
+  // with thunk or promise later on
+  EditTodo: (id: string, text: string): StoreAction<'TodosEdit'> => ({
+    type: 'TodosEdit',
+    payload: { id, text }
+  })
+}
+
+export const action2 = proxyActionCreators<
+  StoreActionCatalog,
+  typeof rewiredActionCreators
+>(rewiredActionCreators)
+
+// Or write in a conventional way
 export const editTodo = (
   id: string,
   text: string
